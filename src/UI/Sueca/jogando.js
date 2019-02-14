89ecf33bd2f3179ac2bb81acc37fed0f24588d78
Leonardo Ribeiro <leonardo.ribeiro@DIRETORIA-030.vbs.corp>
2019-02-14 10:58:10 -0200

    state fixed

--- src/UI/Sueca/jogando.js
+++ src/UI/Sueca/jogando.js
@@ -3,9 +3,40 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import icon from '../../images/DKG-rules.png';
 // import styles from './styles';
+const cartas = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+]
 
 export default class Sueca extends Component {
-  render() {
+  
+    state={
+        showing: 0,
+    }
+  
+  
+    tirarCarta(){
+        console.log('ta aqui')
+        const RandomNumber = Math.floor(Math.random() * cartas.length) ; 
+        console.log(RandomNumber)
+        console.log('aa', cartas[RandomNumber])
+        this.setState({showing: cartas[RandomNumber]},() => {
+            console.log('aq', this.state.showing)
+        })
+    }
+  
+    render() {
     return (
         <View style={styles.container}>
             <View style={styles.head}>
@@ -13,8 +44,12 @@ export default class Sueca extends Component {
                 <TouchableOpacity>
                 <Image style={styles.icon} source={icon}/>
                 </TouchableOpacity>
-
             </View>
+
+            <TouchableOpacity style={styles.TiraCarta}
+                onPress={() => this.tirarCarta()}>
+                <Text style={styles.carta}>{this.state.showing}</Text>
+            </TouchableOpacity>
         </View>
     );
   }
@@ -43,5 +78,13 @@ const styles = StyleSheet.create({
         width:30,
         alignSelf: 'flex-end',
         marginLeft: 10
+    },
+    TiraCarta:{
+        flex: 1
+    },
+    carta:{
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#000'
     }
 })
